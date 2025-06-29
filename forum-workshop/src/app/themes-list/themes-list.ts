b9ae0a462774d32ade1288dfe53747f406a6d67e
Christopher Totlyakov <laminex0622@gmail.com>
2025-06-29 19:25:45 +0300

    add dynamic theme with fetch

--- forum-workshop/src/app/themes-list/themes-list.ts
+++ forum-workshop/src/app/themes-list/themes-list.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Api } from '../api';
+import { Theme } from '../types/theme';
 
 @Component({
   selector: 'app-themes-list',
@@ -9,14 +10,16 @@ import { Api } from '../api';
 })
 export class ThemesList implements OnInit {
 
+  themes: Theme[] =[];
+  
   constructor(private api: Api) {
-
-    
   }
 
   ngOnInit(): void {
-    this.api.getThemes().subscribe((d) => {
-      console.log(d)
+    console.log('test');
+    this.api.getThemes().subscribe((themes) => {
+      console.log(themes);
+      this.themes = themes;
     })
   }
   
